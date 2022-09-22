0315931d3c8c6bfbbab087233baa4bc3ecf2b254
Mateus <mateus.ribeiro@techsavvy.com.br>
2022-09-22 13:06:23 -0300

    :construction: removing sudo

--- setup_everything.sh
+++ setup_everything.sh
@@ -2,18 +2,18 @@
 apt-get update  # To get the latest package lists
 
 # some installs 
-sudo apt-get install git zsh gnome-tweaks gparted wget -y
-sudo snap install notion-snap postman
+apt-get install git zsh gnome-tweaks gparted wget -y
+snap install notion-snap postman
 
 # installing docker 
 apt-get install ca-certificates gnupg curl lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # installing asdf and nodejs
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
@@ -39,8 +39,8 @@ apt-get install sublime-text -y
 # installing sublime-merge
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-merge
+apt-get update
+apt-get install sublime-merge
 
 
 # installing dbeaver-ce
@@ -50,12 +50,12 @@ snap install mysql-workbench-community -y
 # installing anime-cli hehe
 wget -qO- https://Wiener234.github.io/ani-cli-ppa/KEY.gpg | sudo tee /etc/apt/trusted.gpg.d/ani-cli.asc
 wget -qO- https://Wiener234.github.io/ani-cli-ppa/ani-cli-debian.list | sudo tee /etc/apt/sources.list.d/ani-cli-debian.list
-sudo apt update
-sudo apt install ani-cli
+apt update
+apt install ani-cli
 
 # installing JetBrains Mono
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
 mv JetBrainsMono-2.242.zip ~/Downloads/JetBrainsMono-2.242.zip
-sudo mkdir ~/.local/share/fonts
+mkdir ~/.local/share/fonts
 unzip ~/Downloads/JetBrainsMono-2.242.zip -d ~/.local/share/fonts
-
+cp ~/.local/share/fonts/ttf/*.ttf ~/.local/share/fonts/ttf
